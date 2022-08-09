e213b5cac340418968204ce775f7cce91a02fa3f
Futaba06i <104682253+Futaba06i@users.noreply.github.com>
2022-08-09 12:16:09 +0900

    Update 03_C.py

--- AOJ/03/03_C.py
+++ AOJ/03/03_C.py
@@ -1,6 +1,6 @@
 while True:
     l = list(map(int, input().split()))
-    if l[0] == 0 and y[1] == 0:
-     break
     l.sort()
-print(f"l[0], l[1]")
+    if l[0] == 0 and l[1] == 0:
+        break
+    print(l[0], l[1])
