fae52d05dc951ca39967776c6795a6f755c0c0d3
Futaba06i <104682253+Futaba06i@users.noreply.github.com>
2022-08-11 13:17:58 +0900

    Update 06_B.py

---
+++ AOJ/06/06_B.py
@@ -0,0 +1,12 @@
+n = int(input())
+cards = [[False for i in range(13)]for j in range(4)]
+pattern = ["S", "H", "C", "D"]
+for i in range(n):
+    ch, rank = map(str, input().split())
+    r = int(rank)
+    cards[pattern.index(ch)][r-1] = True
+
+for i in range(4):
+    for j in range(13):
+        if cards[i][j] = False:
+            print(pattern[i], j+1)
